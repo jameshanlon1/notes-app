ea669330ce4a051210182096f4a73a0bf2b2bd39
james <jameshanlonrcs@gmail.com>
2023-03-09 15:40:26 +0000

    XML persistance

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,9 @@ dependencies {
     // dependencies for logging
     implementation("io.github.microutils:kotlin-logging:2.1.23")
     implementation("org.slf4j:slf4j-simple:1.7.36")
+    implementation("com.thoughtworks.xstream:xstream:1.4.18")
+    implementation("org.codehaus.jettison:jettison:1.4.1")
+
 }
 
 
