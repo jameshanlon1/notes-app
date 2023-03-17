378049ecd21cb2f4ef20cf9934cb035c45f04736
james <jameshanlonrcs@gmail.com>
2023-03-17 14:18:09 +0000

    Lambdas on listing added

--- src/test/kotlin/controllers/NoteAPITest.kt
+++ src/test/kotlin/controllers/NoteAPITest.kt
@@ -84,13 +84,13 @@ class NoteAPITest {
 
         @Test
         fun `listAllNotes returns Notes when ArrayList has notes stored`() {
-            assertEquals(2, populatedNotes!!.numberOfNotes())
+            assertEquals(5, populatedNotes!!.numberOfNotes())
             val notesString = populatedNotes!!.listAllNotes().lowercase()
             assertTrue(notesString.contains("learning kotlin"))
-            assertFalse(notesString.contains("code app"))
+            assertTrue(notesString.contains("code app"))
             assertTrue(notesString.contains("test app"))
-            assertFalse(notesString.contains("swim"))
-            assertFalse(notesString.contains("summer holiday"))
+            assertTrue(notesString.contains("swim"))
+            assertTrue(notesString.contains("summer holiday"))
         }
 
 
@@ -327,6 +327,39 @@ class NoteAPITest {
             assertTrue(populatedNotes!!.findNote(1)!!.isNoteArchived)
         }
     }
+
+    @Nested
+    inner class CountingMethods {
+
+        @Test
+        fun numberOfNotesCalculatedCorrectly() {
+            assertEquals(5, populatedNotes!!.numberOfNotes())
+            assertEquals(0, emptyNotes!!.numberOfNotes())
+        }
+
+        @Test
+        fun numberOfArchivedNotesCalculatedCorrectly() {
+            assertEquals(2, populatedNotes!!.numberOfArchivedNotes())
+            assertEquals(0, emptyNotes!!.numberOfArchivedNotes())
+        }
+
+        @Test
+        fun numberOfActiveNotesCalculatedCorrectly() {
+            assertEquals(3, populatedNotes!!.numberOfActiveNotes())
+            assertEquals(0, emptyNotes!!.numberOfActiveNotes())
+        }
+
+        @Test
+        fun numberOfNotesByPriorityCalculatedCorrectly() {
+            assertEquals(1, populatedNotes!!.numberOfNotesByPriority(1))
+            assertEquals(0, populatedNotes!!.numberOfNotesByPriority(2))
+            assertEquals(1, populatedNotes!!.numberOfNotesByPriority(3))
+            assertEquals(2, populatedNotes!!.numberOfNotesByPriority(4))
+            assertEquals(1, populatedNotes!!.numberOfNotesByPriority(5))
+            assertEquals(0, emptyNotes!!.numberOfNotesByPriority(1))
+        }
+    }
+
 
 }
 
